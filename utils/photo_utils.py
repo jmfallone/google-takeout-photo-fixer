9000eba7b04e62534686ff04da0ddcee55af7a01
Joey Fallone <j.fallone@ualberta.ca>
2024-07-14 18:33:50 -0600

    feat: utils for photos

--- utils/photo_utils.py
+++ utils/photo_utils.py
@@ -3,22 +3,35 @@ from datetime import datetime
 
 def update_photo_creation_date(photo_file_path: str, new_date: datetime) -> None:
     """
-    Modifies the creation date of a photo file using the pyexiv2 library. 
+    Updates the EXIF creation date of a photo to a new date.
 
-    :param: photo_file_path: str - the file path being modified
-    :param: new_date: datetime - the new datetime to apply for creation date
+    This function modifies the EXIF metadata of a photo file to update its creation date.
+    It changes the `DateTimeOriginal`, `DateTimeDigitized`, and `DateTime` fields to the
+    new specified date.
+
+    Parameters:
+    photo_file_path (str): The file path to the photo whose creation date needs to be updated.
+    new_date (datetime): The new creation date to set in the EXIF metadata.
+
+    Returns:
+    None
+
+    Example:
+    >>> from datetime import datetime
+    >>> new_date = datetime(2020, 5, 17, 10, 30, 45)
+    >>> update_photo_creation_date('photo.jpg', new_date)
+    Updated creation date to 2020:05:17 10:30:45 for photo.jpg
+
+    The function will print a message indicating the new creation date and the file path of the updated photo.
     """
     formatted_date: str = new_date.strftime('%Y:%m:%d %H:%M:%S')
 
-    # Load the existing EXIF data
     exif_dict = piexif.load(photo_file_path)
 
-    # Update the creation date in EXIF data
     exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = formatted_date.encode()
     exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = formatted_date.encode()
     exif_dict['0th'][piexif.ImageIFD.DateTime] = formatted_date.encode()
 
-    # Convert the EXIF data back to bytes and insert into the image
     exif_bytes = piexif.dump(exif_dict)
     piexif.insert(exif_bytes, photo_file_path)
     print(f"Updated creation date to {formatted_date} for {photo_file_path}")
--- utils/takeout_utils.py
+++ utils/takeout_utils.py
@@ -1,1 +1,43 @@
-# def 
+import json
+from datetime import datetime, timezone
+
+def get_takeout_datetime(json_file_path):
+    """
+    Extracts the photo taken datetime from a JSON file containing Google Takeout photo metadata.
+
+    This function reads a JSON file, parses it to extract the `photoTakenTime` timestamp, and converts
+    the timestamp to a `datetime` object.
+
+    Parameters:
+    json_file_path (str): The file path to the JSON file containing the photo metadata.
+
+    Returns:
+    datetime: A datetime object representing when the photo was taken.
+
+    Example:
+    >>> photo_datetime = get_takeout_datetime('photo_data.json')
+    >>> print(photo_datetime)
+    2017-08-20 21:51:22
+
+    The JSON file should have a structure similar to:
+    {
+      "title": "IMG_20170820_155122.jpg",
+      "description": "",
+      "imageViews": "16",
+      "creationTime": {
+        "timestamp": "1503270137",
+        "formatted": "20 Aug 2017, 23:02:17 UTC"
+      },
+      "photoTakenTime": {
+        "timestamp": "1503265882",
+        "formatted": "20 Aug 2017, 21:51:22 UTC"
+      },
+      ...
+    }
+    """
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+        
+    photo_taken_timestamp = int(data['photoTakenTime']['timestamp'])
+    
+    return datetime.fromtimestamp(photo_taken_timestamp, tz=timezone.utc)
